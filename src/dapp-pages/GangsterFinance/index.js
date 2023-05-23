0824504d21a46214916b3ce149c8868cb5a49fe4
jewelas <jewelas.tm@gmail.com>
2023-05-23 04:26:08 -0400

    fix gangster finance

--- src/dapp-pages/GangsterFinance/index.js
+++ src/dapp-pages/GangsterFinance/index.js
@@ -6,11 +6,6 @@ import EventCard from '../../dapp-components/GFI/EventCard'
 export default function GangsterFinanceTeamUp() {
   return (
     <>
-      {/* <Grid container className="d-flex">
-        <Grid item>
-          <EventCard />
-        </Grid>
-      </Grid> */}
       <div className="d-flex justify-content-center">
         <EventCard />
       </div>
